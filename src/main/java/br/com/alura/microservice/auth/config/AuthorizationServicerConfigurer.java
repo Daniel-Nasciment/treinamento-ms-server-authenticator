3c889dc5f3e2d1ade0b8ccd3dc0ffc6f94082e15
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-07-27 17:48:17 -0300

    Tecnologias plugadas

--- src/main/java/br/com/alura/microservice/auth/config/AuthorizationServicerConfigurer.java
+++ src/main/java/br/com/alura/microservice/auth/config/AuthorizationServicerConfigurer.java
@@ -1,15 +1,48 @@
 package br.com.alura.microservice.auth.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 public class AuthorizationServicerConfigurer extends AuthorizationServerConfigurerAdapter {
 
+	// CONFIGURAÇÕES OAUTH
+	
+	@Autowired
 	private AuthenticationManager authenticationManager;
-	
+
+	@Autowired
 	private UserDetailsService userDetailsService;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
+	// NO EXEMPLO DO POSTMAN, O QUE FICA NA GUIA AUTHORIZATION SÃO AS INFORMAÇÕES (USERNAME, PASSOWRD) DA APLICAÇÃO PARA 
+	// AUTENTICAR O USUÁRIO
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+		// CRIAÇÃO DA APLICAÇÃO QUE FARA A REQUISIÇÃO DE AUTENTICAÇÃO DO USUÁRIO EM MEMÓRIA
+		
+		clients.inMemory()
+			.withClient("loja")
+			.secret(passwordEncoder.encode("loja123"))
+			.authorizedGrantTypes("password")
+			.scopes("web", "mobile");
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+
+		endpoints.authenticationManager(authenticationManager).userDetailsService(userDetailsService);
+
+	}
+
 }
--- src/main/java/br/com/alura/microservice/auth/config/WebSecurityConfigurer.java
+++ src/main/java/br/com/alura/microservice/auth/config/WebSecurityConfigurer.java
@@ -3,6 +3,7 @@ package br.com.alura.microservice.auth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +11,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+	
+	// CONFIGURAÇÕES SPRING SECURITY
 
 	@Override
 	@Bean
@@ -28,4 +31,16 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		// CRIAÇÃO DE UM USUARIO EM MEMÓRIA
+		
+		auth.inMemoryAuthentication()
+		.passwordEncoder(passwordEncoder())
+		.withUser("daniel")
+		.password(passwordEncoder().encode("1234"))
+		.roles("USER");
+	}
+	
 }
